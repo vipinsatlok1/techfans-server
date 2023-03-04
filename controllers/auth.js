da05f654a16a60e8c30bd64ebe11c0c6bb96b48c
vipinsatlok1 <126874481+vipinsatlok1@users.noreply.github.com>
2023-03-04 09:50:17 +0530

    send token manually in login function

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,7 +25,7 @@ class Auth {
           maxAge: 365 * 24 * 60 * 60 * 1000,
           httpOnly: true,
         })
-        .send(user);
+        .send(user, token);
     } catch (err) {
       next(err);
     }
@@ -54,7 +54,7 @@ class Auth {
           maxAge: 365 * 24 * 60 * 60 * 1000,
           httpOnly: true,
         })
-        .send(user);
+        .send(user, token);
     } catch (err) {
       next(err);
     }
